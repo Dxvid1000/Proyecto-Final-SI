e5d919f23b283e9a61bdfc162a7570417056e819
Marcska <marcska97@gmail.com>
2020-06-16 00:04:37 -0500

    Se actualiza archivo de alta

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/alta',(req,res,next)=>{
+  res.render('alta_banda',{});
+});
 module.exports = router;
