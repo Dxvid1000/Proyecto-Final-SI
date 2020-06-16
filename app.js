d792b107a53cbcec39b6a5253f818e2bcca5dc42
Dxvid1000 <davidandres219@gmail.com>
2020-06-16 16:56:51 -0500

    correcciones

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/grupo', grupoRouter)
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handle 
 app.use(function(req, res, next) {
   next(createError(404));
 });
