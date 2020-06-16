a5bc3ecd01b7fc419d9537d9358d39bbeb4463ae
Marcska <marcska97@gmail.com>
2020-06-15 22:27:02 -0500

    se cambia de jade a hbs

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ mongoose.connect('mongodb+srv://DavidV:HeZAL0seEaaSKa7H@gruposmusicales-mdtfk.mo
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
